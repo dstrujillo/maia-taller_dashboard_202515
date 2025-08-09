1e96439b6016c6e862730762bb7c09a55cb8f470
dstrujillo <dstrujilloa@gmail.com>
2025-08-09 15:42:11 -0500

    Ajuste título gráfico x2

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard energia mejorado"
+app.title = "Dashboard energia mejorado con PR"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
